29e5040dc3c60b82e3e929f412076cb7272bfb4c
binshadI <binshadbinshad729@gmail.com>
2025-12-03 21:51:50 +0530

    working on project

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const app = require('./app');
+
+const port = process.env.PORT || 8080;
+
+app.listen(port,()=>{
+    console.log(`server is running on prot ${port}`);
+    
+});
